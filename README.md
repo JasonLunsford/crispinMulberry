600302aaf19b2c61a64a40bb8adaff5ef56e0f1d
Jason Lunsford <miyamota@yahoo.com>
2013-11-28 12:28:55 -0500

    browse categories and recent articles finished

---
+++ README.md
@@ -0,0 +1,6 @@
+Reference:
+http://rogerdudler.github.io/git-guide/
+
+git add .
+git commit -m "my message"
+git push origin master
--- js/main.js
+++ js/main.js
@@ -10,13 +10,7 @@
 		// Default rotation speed: 4000
 		// Usage: natCarousel({ speed:[your_rotation_value_in_milliseconds_integer_only] })
 		//$("#carouselRoot").natCarousel();
-		
-		// Disable nav items
-		$("nav li a").on("click", function(e) {
-			e.preventDefault();
-			console.log("Main nav item clicked.");
-		});
-		
+				
 		// Simple mock-server demonstration highlighting how the menu system could work, and the portability of this
 		// particular CSS solution.
 		$("nav li").on("click",function() {
@@ -26,10 +20,30 @@
 			( $(this).hasClass("lastChild") ) ? $(this).addClass("selected lastChildSelected") : $(this).addClass("selected");
 		});
 		
+		// Disable nav items
+		$("nav li a").on("click", function(e) {
+			e.preventDefault();
+			console.log($(this).text()+" clicked");
+		});
+		
+		// Disable page footer icons when JS is available - otherwise open page in another tab
+		$("div.siteFooter a").on("click", function(e) {
+			e.preventDefault();
+			if ( $(this).attr(src) ) { console.log("Link to "+$(this).attr(src)+" clicked."); }
+			else { console.log($(this).text()+" clicked"); }
+		});
+		
+		
 		// Intercept and deactivate form submissions
 		$("#searchButton").on("click", function(e) {
 			e.preventDefault();
 			console.log("Site search triggered.");
+		});
+		
+		$("#newsletterButton").on("click", function(e) {
+			e.preventDefault();
+			// add click state detection for the two check boxes and output their state
+			console.log("Newsletter lead capture form submitted");
 		});
 	
 	});
